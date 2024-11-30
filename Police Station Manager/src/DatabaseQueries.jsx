8eec5e866a7a5f2d9a07ef604e2fcf515c886b65
Ethan <ethan.bhalla@gmail.com>
2024-11-30 14:47:34 -0500

    Added .env files

--- Police Station Manager/src/DatabaseQueries.jsx
+++ Police Station Manager/src/DatabaseQueries.jsx
@@ -1,12 +1,13 @@
 // Import the mysql2 package
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: '', // e.g., 'localhost' or your EC2 public IP address
-  user: '', // e.g., 'root'
-  password: '',
-  database: '', // e.g., 'mydatabase'
+  host: process.env.DB_HOST, // e.g., 'localhost' or your EC2 public IP address
+  user: process.env.DB_USER, // e.g., 'root'
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, // e.g., 'mydatabase'
 });
 
 // Connect to the database
@@ -16,22 +17,17 @@ connection.connect((err) => {
     return;
   }
   console.log('Connected as id ' + connection.threadId);
+
+  // Example query
+  const query = 'SELECT * FROM List;';
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.error('Query error: ', err);
+      return;
+    }
+    console.log('Results: ', results);
+  });
+
+  // Close the connection
+  connection.end();
 });
-
-
-/*
-const createTableQuery = `
-SELECT * FROM List;
-`;
-
-// Example query
-connection.query(createTableQuery, (err, results, fields) => {
-  if (err) {
-    console.error('Query error: ', err);
-    return;
-  }
-  console.log('Results: ', results);
-});*/
-
-// Close the connection
-connection.end();
